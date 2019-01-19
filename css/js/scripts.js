6f5622b981a9879e7630f9776a6294f35abe4211
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-01-19 20:42:11 +0200

    add id

--- css/js/scripts.js
+++ css/js/scripts.js
@@ -35,23 +35,33 @@
 // };
 
 // Business-Logic
-
+var student;
 $(document).ready(function(){
     $("button#new").click(function(){
     $(".gikweri").hide();
     $(".container1").show();
 
+    var name =$('#namee').val();
+    
+    $('#dg').text(name);
 
-var name =$('#namee').val();
-console.log(name)
- $('#dg').text(name);
+    var student =$('#initial').val();
+    console.log(student)
+    $('#mg').text(student);
 
- var student =$('#initial').val();
-console.log(student)
- $('#mg').text(student);
- 
- });
+    $("#submit").click(function(){
+        var kub = parseInt($('#kubitsa').val());
+        var withdr = parseInt($('#kubikuza').val());
+        
+        var sum = kub + parseInt(student);
+        $('#mg').text(sum);
+        // var sub =parseInt(student)-withdr;
+        // $('#mg').text(sub);
 
+       
+    });
+
+});
 
 });
 
